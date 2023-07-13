f2997ce574190b2ebdf5249010c2937af77775a3
KirtankumarPatel <133532645+KirtankumarPatel@users.noreply.github.com>
2023-07-13 14:08:06 -0400

    add comment
    we add comments in java code.

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,8 @@
 package com.kirtanpatel.todolist;
 
+
+// here we import compactactivity,bundle, view then array adapter,button,textview,edittext,listview
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -15,62 +18,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
-    public class MainActivity extends AppCompatActivity {
+// we create public class and using private function like edittext,button,listview,arraylist,array adapter..
 
-        private EditText editTextTask;
-        private Button buttonAdd;
-        private ListView listViewTasks;
-        private ArrayList<String> tasksList;
-        private ArrayAdapter<String> adapter;
+public class MainActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    private EditText editTextTask;
+    private Button buttonAdd;
+    private ListView listViewTasks;
+    private ArrayList<String> tasksList;
+    private ArrayAdapter<String> adapter;
 
-            // Initialize views
-            editTextTask = findViewById(R.id.editTextTask);
-            buttonAdd = findViewById(R.id.buttonAdd);
-            listViewTasks = findViewById(R.id.listViewTasks);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            // Initialize task list and adapter
-            tasksList = new ArrayList<>();
-            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_checked, tasksList);
-            listViewTasks.setAdapter(adapter);
+        // Initialize views
+        editTextTask = findViewById(R.id.editTextTask);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        listViewTasks = findViewById(R.id.listViewTasks);
 
-            // Add button click listener
-            buttonAdd.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    addTask();
-                }
-            });
+        // Initialize task list and adapter
+        tasksList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_checked, tasksList);
+        listViewTasks.setAdapter(adapter);
 
-            // List item click listener
-            listViewTasks.setOnItemClickListener((parent, view, position, id) -> {
-                CheckedTextView checkedTextView = (CheckedTextView) view;
-                checkedTextView.setChecked(!checkedTextView.isChecked());
-                tasksList.set(position, checkedTextView.getText().toString());
-            });
-
-            // List item long click listener
-            listViewTasks.setOnItemLongClickListener((parent, view, position, id) -> {
-                tasksList.remove(position);
-                adapter.notifyDataSetChanged();
-                Toast.makeText(getApplicationContext(), "Task removed", Toast.LENGTH_SHORT).show();
-                return true;
-            });
-        }
-
-        private void addTask() {
-            String task = editTextTask.getText().toString().trim();
-
-            if (!task.isEmpty()) {
-                tasksList.add(task);
-                adapter.notifyDataSetChanged();
-                editTextTask.setText("");
-            } else {
-                Toast.makeText(this, "Enter a new items", Toast.LENGTH_SHORT).show();
+        // Add button click listener
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTask();
             }
+        });
+
+        // List item click listener
+        listViewTasks.setOnItemClickListener((parent, view, position, id) -> {
+            CheckedTextView checkedTextView = (CheckedTextView) view;
+            checkedTextView.setChecked(!checkedTextView.isChecked());
+            tasksList.set(position, checkedTextView.getText().toString());
+        });
+
+        // List item long click listener
+        listViewTasks.setOnItemLongClickListener((parent, view, position, id) -> {
+            tasksList.remove(position);
+            adapter.notifyDataSetChanged();
+            Toast.makeText(getApplicationContext(), "Task removed", Toast.LENGTH_SHORT).show();
+            return true;
+        });
+    }
+
+
+// called private void function and using if else conditions
+
+
+    private void addTask() {
+        String task = editTextTask.getText().toString().trim();
+
+        if (!task.isEmpty()) {
+            tasksList.add(task);
+            adapter.notifyDataSetChanged();
+            editTextTask.setText("");
+        } else {
+            Toast.makeText(this, "Enter a new items", Toast.LENGTH_SHORT).show();
         }
     }
+}
